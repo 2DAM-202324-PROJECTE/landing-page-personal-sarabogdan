d8e7b5f28267572f7ce25ed094930d12e562e085
«sara21h» <sbogdan@iesebre.com>
2023-11-07 20:26:08 +0100

    ultims projectes afegits + alguns canvis

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: 'pulse 0.9s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      fontSize: {
+        '2.5xl': '1.75rem',
+      },
+    },
   },
   plugins: [],
 }
